aff96a88585daa481329a0642b2a7b567cca3062
Pasin-Boonsermsuwong <panbemail@gmail.com>
2015-06-15 11:33:23 +0700

    Move other projects to backup

--- Unity_2/Assets/Script/BoundaryCollision.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class BoundaryCollision : MonoBehaviour {
-
-	void OnTriggerExit2D(Collider2D other){
-//		Debug.Log("Exit Boundary");
-		Destroy(other.gameObject);
-	}
-}
--- Unity_2/Assets/Script/BulletData.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class BulletData : MonoBehaviour {
-
-	public float Damage;
-}
