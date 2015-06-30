aef4168ea938d5331151f1bf1b92244aadeb4185
Pasin-Boonsermsuwong <panbemail@gmail.com>
2015-06-30 11:03:12 +0700

    photonetwork / gitignore for resource

---
+++ Unity_2/Assets/RotateTurret.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTurret : MonoBehaviour {
+
+	Transform myTransform;
+//	Transform rootTransform;
+	Transform cameraTransform;
+
+	void Start () {
+		myTransform = GetComponent<Transform>();
+	//	rootTransform = ;
+		cameraTransform = myTransform.root.Find("FirstPersonCharacter");
+	}
+
+	void Update () {
+		myTransform.rotation = cameraTransform.rotation;
+			//Quaternion.Euler(new Vector3(rootTransform.eulerAngles.x,cameraTransform.rotation.eulerAngles.y,rootTransform.eulerAngles.z));
+	}
+}
