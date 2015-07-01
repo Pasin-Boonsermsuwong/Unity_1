21e045c50e7c56d403045db57514922797580592
Pasin-Boonsermsuwong <panbemail@gmail.com>
2015-07-01 12:27:44 +0700

    sync
    chsaracter move based on rigidbody

---
+++ Unity_2/Assets/FirstPersonControllerForce.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.Characters.FirstPerson;
+using Random = UnityEngine.Random;
+
+public class FirstPersonControllerForce : MonoBehaviour{
+		
+	MouseLook m_MouseLook;
+	Camera cam;
+	Transform camTransform;
+	Rigidbody rb;
+
+	bool isWalking;
+
+	Vector3 moveDir;
+
+	public float walkSpeed;
+	public float runSpeed;
+	float speed;
+
+
+    // Use this for initialization
+     void Start(){
+		cam = GetComponentInChildren<Camera>();
+		camTransform = cam.GetComponent<Transform>();
+		rb = GetComponent<Rigidbody>();
+		m_MouseLook.Init(transform , cam.transform);
+    }
+
+     void Update(){
+		RotateView();
+    }
+
+     void PlayLandingSound(){
+
+    }
+
+     void FixedUpdate(){
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+		bool waswalking = isWalking;
+		isWalking = !Input.GetButton("Walk");
+		speed = isWalking ? walkSpeed : runSpeed;
+
+
+
+		moveDir = new Vector3(horizontal, 0f ,vertical);
+
+		if (moveDir.sqrMagnitude > 1){
+			moveDir.Normalize();
+		}
+		rb.AddForce(moveDir);
+    }
+
+    void PlayJumpSound(){
+
+    }
+
+    void RotateView(){
+		m_MouseLook.LookRotation (transform, camTransform);
+    }
+
+
+}
