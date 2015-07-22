cf9b1ac1a558d39fcab0a1e90d94bbca20a786d9
Pasin-Boonsermsuwong <panbemail@gmail.com>
2015-07-22 21:42:08 +0700

    move first project to backup

--- Unity_1/Assets/Scripts/RandomForce.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class RandomForce : MonoBehaviour {
-	public float force;
-	void Start () {
-		GetComponent<Rigidbody>().AddForce(Random.insideUnitSphere * force);
-	}
-}
